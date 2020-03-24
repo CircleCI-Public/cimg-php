f70e0bdc718107fed821b9d866871c0cfc486793
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-23 22:01:27 -0400

    Respin PHP v5.6.40 to enable node variant. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 7.2/Dockerfile -t cimg/php:7.2.29  -t cimg/php:7.2 .
+docker build --file 5.6/Dockerfile -t cimg/php:5.6.40  -t cimg/php:5.6 .
+docker build --file 5.6/node/Dockerfile -t cimg/php:5.6.40-node  -t cimg/php:5.6-node .
