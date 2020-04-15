8d1bf9451437f81f902db73a03c9c5ac0b8af482
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-04-15 18:12:12 -0400

    Add v7.2.30. [release] (#48)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.5  -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.5-node  -t cimg/php:7.4-node .
+docker build --file 7.2/Dockerfile -t cimg/php:7.2.30  -t cimg/php:7.2 .
+docker build --file 7.2/node/Dockerfile -t cimg/php:7.2.30-node  -t cimg/php:7.2-node .
