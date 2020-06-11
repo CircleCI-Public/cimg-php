65cf1a8cb2358627fa61921b28f16d6cea774aba
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-06-11 13:27:28 -0400

    Publish v7.3.19 and v7.4.7. [release] (#59)

--- build-images.sh
+++ build-images.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 5.6/Dockerfile -t cimg/php:5.6.40  -t cimg/php:5.6 .
-docker build --file 5.6/node/Dockerfile -t cimg/php:5.6.40-node  -t cimg/php:5.6-node .
-docker build --file 7.2/Dockerfile -t cimg/php:7.2.31  -t cimg/php:7.2 .
-docker build --file 7.2/node/Dockerfile -t cimg/php:7.2.31-node  -t cimg/php:7.2-node .
-docker build --file 7.3/Dockerfile -t cimg/php:7.3.18  -t cimg/php:7.3 .
-docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.18-node  -t cimg/php:7.3-node .
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.6  -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.6-node  -t cimg/php:7.4-node .
+docker build --file 7.3/Dockerfile -t cimg/php:7.3.19  -t cimg/php:7.3 .
+docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.19-node  -t cimg/php:7.3-node .
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.7  -t cimg/php:7.4 .
+docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.7-node  -t cimg/php:7.4-node .
