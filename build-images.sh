3638edca0c5d048d2a390c60c903fd1440132465
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-07-09 11:05:23 -0400

    Publish v7.4.8. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 7.2/Dockerfile -t cimg/php:7.2.32  -t cimg/php:7.2 .
-docker build --file 7.2/node/Dockerfile -t cimg/php:7.2.32-node  -t cimg/php:7.2-node .
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.8  -t cimg/php:7.4 .
+docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.8-node  -t cimg/php:7.4-node .
