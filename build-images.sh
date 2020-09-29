e907a1c84871cbc57e4910e5b51f99f34d4e5ff0
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-09-29 19:09:47 -0400

    Publish v7.3.23 and v7.4.11. [release] (#71)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 7.3/Dockerfile -t cimg/php:7.3.22  -t cimg/php:7.3 .
-docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.22-node  -t cimg/php:7.3-node .
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.10  -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.10-node  -t cimg/php:7.4-node .
+docker build --file 7.3/Dockerfile -t cimg/php:7.3.23  -t cimg/php:7.3 .
+docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.23-node  -t cimg/php:7.3-node .
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.11  -t cimg/php:7.4 .
+docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.11-node  -t cimg/php:7.4-node .
