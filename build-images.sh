cf8a118302e9abc3e8dc75cdc29c994d723dbe14
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-13 18:32:55 -0500

    Respin browsers tags for xvfb, part 2. [release] (#89)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 5.6/browsers/Dockerfile -t cimg/php:5.6.40-browsers  -t cimg/php:5.6-browsers .
-docker build --file 7.2/browsers/Dockerfile -t cimg/php:7.2.34-browsers  -t cimg/php:7.2-browsers .
-docker build --file 7.3/browsers/Dockerfile -t cimg/php:7.3.23-browsers  -t cimg/php:7.3-browsers .
-docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.11-browsers  -t cimg/php:7.4-browsers .
+docker build --file 7.3/browsers/Dockerfile -t cimg/php:7.3.24-browsers  -t cimg/php:7.3-browsers .
+docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.12-browsers  -t cimg/php:7.4-browsers .
