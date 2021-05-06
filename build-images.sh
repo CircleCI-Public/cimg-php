224b462128f12b32267e8618269178e18401ede1
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-05-06 11:49:14 -0400

    Publish v8.0.6. [release] (#110)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.19  -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.19-node  -t cimg/php:7.4-node .
-docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.19-browsers  -t cimg/php:7.4-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.6  -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.6-node  -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.6-browsers  -t cimg/php:8.0-browsers .
