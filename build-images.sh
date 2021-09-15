9089b8c6de3bdf6be0f77693cec2bd1f5fdd45aa
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2021-09-14 21:30:39 -0700

    Publish v7.3.30, v7.4.23, and v8.0.10. [release] (#118)
    Co-authored-by: Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.9  -t cimg/php:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.9-node  -t cimg/php:8.0-node .
-docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.9-browsers  -t cimg/php:8.0-browsers .
+docker build --file 7.3/Dockerfile -t cimg/php:7.3.30  -t cimg/php:7.3 .
+docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.30-node  -t cimg/php:7.3-node .
+docker build --file 7.3/browsers/Dockerfile -t cimg/php:7.3.30-browsers  -t cimg/php:7.3-browsers .
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.23  -t cimg/php:7.4 .
+docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.23-node  -t cimg/php:7.4-node .
+docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.23-browsers  -t cimg/php:7.4-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.10  -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.10-node  -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.10-browsers  -t cimg/php:8.0-browsers .
