82e75e8bce95dc11bd256a8859fd0fc5f3e8abb9
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-09-21 13:42:47 -0400

    Publish v7.3.31, v7.4.24, and v8.0.11. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-docker build --file 7.0/Dockerfile -t cimg/php:7.0.33  -t cimg/php:7.0 .
-docker build --file 7.0/node/Dockerfile -t cimg/php:7.0.33-node  -t cimg/php:7.0-node .
-docker build --file 7.0/browsers/Dockerfile -t cimg/php:7.0.33-browsers  -t cimg/php:7.0-browsers .
-docker build --file 7.1/Dockerfile -t cimg/php:7.1.33  -t cimg/php:7.1 .
-docker build --file 7.1/node/Dockerfile -t cimg/php:7.1.33-node  -t cimg/php:7.1-node .
-docker build --file 7.1/browsers/Dockerfile -t cimg/php:7.1.33-browsers  -t cimg/php:7.1-browsers .
+docker build --file 7.3/Dockerfile -t cimg/php:7.3.31  -t cimg/php:7.3 .
+docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.31-node  -t cimg/php:7.3-node .
+docker build --file 7.3/browsers/Dockerfile -t cimg/php:7.3.31-browsers  -t cimg/php:7.3-browsers .
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.24  -t cimg/php:7.4 .
+docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.24-node  -t cimg/php:7.4-node .
+docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.24-browsers  -t cimg/php:7.4-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.11  -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.11-node  -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.11-browsers  -t cimg/php:8.0-browsers .
