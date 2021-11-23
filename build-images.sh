a72a836339b7ccef15f3625d427f5814e9619e97
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-11-23 15:42:37 -0500

    Enable SOAP p1 [release]

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-docker build --file 7.3/Dockerfile -t cimg/php:7.3.33  -t cimg/php:7.3 .
-docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.33-node  -t cimg/php:7.3-node .
-docker build --file 7.3/browsers/Dockerfile -t cimg/php:7.3.33-browsers  -t cimg/php:7.3-browsers .
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.26  -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.26-node  -t cimg/php:7.4-node .
-docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.26-browsers  -t cimg/php:7.4-browsers .
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.13  -t cimg/php:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.13-node  -t cimg/php:8.0-node .
-docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.13-browsers  -t cimg/php:8.0-browsers .
+docker build --file 7.3/Dockerfile -t cimg/php:7.3.32  -t cimg/php:7.3 .
+docker build --file 7.3/node/Dockerfile -t cimg/php:7.3.32-node  -t cimg/php:7.3-node .
+docker build --file 7.3/browsers/Dockerfile -t cimg/php:7.3.32-browsers  -t cimg/php:7.3-browsers .
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.25  -t cimg/php:7.4 .
+docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.25-node  -t cimg/php:7.4-node .
+docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.25-browsers  -t cimg/php:7.4-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.12  -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.12-node  -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.12-browsers  -t cimg/php:8.0-browsers .
