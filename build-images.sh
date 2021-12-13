2fd39071243a90aa1e5ee9216bd2a4282d6c50e5
Hiroshi Onozaki <hrs-o@users.noreply.github.com>
2021-12-14 01:14:13 +0900

    Add PHP8.1 (#138) [release]
    * Add PHP8.1
    
    `$ bash ./shared/gen-dockerfiles.sh 8.1.0`
    
    * Update Dockerfile
    
    * Update Dockerfile.template
    
    Co-authored-by: Ricardo N Feliciano <FelicianoTech@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 7.2/Dockerfile -t cimg/php:7.2.34  -t cimg/php:7.2 .
-docker build --file 7.2/node/Dockerfile -t cimg/php:7.2.34-node  -t cimg/php:7.2-node .
-docker build --file 7.2/browsers/Dockerfile -t cimg/php:7.2.34-browsers  -t cimg/php:7.2-browsers .
+docker build --file 8.1/Dockerfile -t cimg/php:8.1.0  -t cimg/php:8.1 .
+docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.0-node  -t cimg/php:8.1-node .
+docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.0-browsers  -t cimg/php:8.1-browsers .
