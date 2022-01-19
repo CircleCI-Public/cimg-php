f76a382bc92702ef40d3dc9df0af799fabdce28c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-01-19 18:22:59 -0500

    Publish v8.1.2. [release]

---
+++ push-images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/php:8.1.2
+docker push cimg/php:8.1
+docker push cimg/php:8.1.2-node
+docker push cimg/php:8.1-node
+docker push cimg/php:8.1.2-browsers
+docker push cimg/php:8.1-browsers
