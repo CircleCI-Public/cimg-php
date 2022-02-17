cb10785e908d82508b126918e522c6dd413bf373
Jalexchen <jeff.chen@circleci.com>
2022-02-17 14:22:15 -0800

    Publish v8.0.16. [release] (#155)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.1/Dockerfile -t cimg/php:8.1.2  -t cimg/php:8.1 .
-docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.2-node  -t cimg/php:8.1-node .
-docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.2-browsers  -t cimg/php:8.1-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.16  -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.16-node  -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.16-browsers  -t cimg/php:8.0-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/php:8.1.2
-docker push cimg/php:8.1
-docker push cimg/php:8.1.2-node
-docker push cimg/php:8.1-node
-docker push cimg/php:8.1.2-browsers
-docker push cimg/php:8.1-browsers
+docker push cimg/php:8.0.16
+docker push cimg/php:8.0
+docker push cimg/php:8.0.16-node
+docker push cimg/php:8.0-node
+docker push cimg/php:8.0.16-browsers
+docker push cimg/php:8.0-browsers
