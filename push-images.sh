376af53ee3c3a53def6929dd4a6fccca98194db2
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-03-15 17:07:08 +0000

    Publish v8.0.17. [release] (#161)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/php:8.0.16
+docker push cimg/php:8.0.17
 docker push cimg/php:8.0
-docker push cimg/php:8.0.16-node
+docker push cimg/php:8.0.17-node
 docker push cimg/php:8.0-node
-docker push cimg/php:8.0.16-browsers
+docker push cimg/php:8.0.17-browsers
 docker push cimg/php:8.0-browsers
