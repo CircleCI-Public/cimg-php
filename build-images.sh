3ef0362a3fe5932c857e2b063b1dceec1dc309e7
Jeff Chen <jeff.chen@circleci.com>
2022-05-06 10:14:25 -0700

    mlocati helper scripts

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.29  -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.29-node  -t cimg/php:7.4-node .
-docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.29-browsers  -t cimg/php:7.4-browsers .
+docker build --file 8.1/Dockerfile -t cimg/php:8.1.5  -t cimg/php:8.1 .
+docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.5-node  -t cimg/php:8.1-node .
+docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.5-browsers  -t cimg/php:8.1-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/php:7.4.29
-docker push cimg/php:7.4
-docker push cimg/php:7.4.29-node
-docker push cimg/php:7.4-node
-docker push cimg/php:7.4.29-browsers
-docker push cimg/php:7.4-browsers
+docker push cimg/php:8.1.5
+docker push cimg/php:8.1
+docker push cimg/php:8.1.5-node
+docker push cimg/php:8.1-node
+docker push cimg/php:8.1.5-browsers
+docker push cimg/php:8.1-browsers
