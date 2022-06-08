ddeb2fc03dc4d90f946bacce9c385ce3f4f2ad4d
Jeff Chen <jeff.chen@circleci.com>
2022-06-08 13:54:26 -0700

    publish ext-test images

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.19  -t cimg/php:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.19-node  -t cimg/php:8.0-node .
-docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.19-browsers  -t cimg/php:8.0-browsers .
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.29-ext-test
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.19-ext-test
+docker build --file 8.1/Dockerfile -t cimg/php:8.1.5-ext-test
