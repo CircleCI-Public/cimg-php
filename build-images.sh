92217d69b5d46f3ff35bcaf927b599e162fc36b5
Jeff Chen <jeff.chen@circleci.com>
2022-06-08 14:00:37 -0700

    missing . in build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.29-ext-test
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.19-ext-test
-docker build --file 8.1/Dockerfile -t cimg/php:8.1.5-ext-test
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.29-ext-test .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.19-ext-test .
+docker build --file 8.1/Dockerfile -t cimg/php:8.1.5-ext-test .
