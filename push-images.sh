79ba9b1f46380e6e7943360e5e8ee596b5c0e818
Jeff Chen <jeff.chen@circleci.com>
2022-06-14 11:29:52 -0700

    reverting to solve conflicts

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/php:7.4.29-ext-test
-docker push cimg/php:8.0.19-ext-test
-docker push cimg/php:8.1.5-ext-test
+docker push cimg/php:8.0.19
+docker push cimg/php:8.0
+docker push cimg/php:8.0.19-node
+docker push cimg/php:8.0-node
+docker push cimg/php:8.0.19-browsers
+docker push cimg/php:8.0-browsers
