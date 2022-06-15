105022c37159ae6c6d20e6cf3d0f8924f812a966
Jeff Chen <jeff.chen@circleci.com>
2022-06-15 13:29:54 -0700

    reverts dockerfiles to previous commit

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
+docker push cimg/php:7.4.30
+docker push cimg/php:7.4
+docker push cimg/php:7.4.30-node
+docker push cimg/php:7.4-node
+docker push cimg/php:7.4.30-browsers
+docker push cimg/php:7.4-browsers
+
+docker push cimg/php:8.0.20
+docker push cimg/php:8.0
+docker push cimg/php:8.0.20-node
+docker push cimg/php:8.0-node
 docker push cimg/php:8.0.20-browsers
 docker push cimg/php:8.0-browsers
 
@@ -10,4 +21,3 @@ docker push cimg/php:8.1.7-node
 docker push cimg/php:8.1-node
 docker push cimg/php:8.1.7-browsers
 docker push cimg/php:8.1-browsers
-
