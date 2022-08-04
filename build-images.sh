ba52d4d07dd752849cc7ec1a1be1a0ab6ca2bb58
Jalexchen <jeff.chen@circleci.com>
2022-08-04 16:42:28 -0700

    Publish v8.0.22 and v8.1.9. [release] (#179)

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.30  -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.30-node  -t cimg/php:7.4-node .
-docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.30-browsers  -t cimg/php:7.4-browsers .
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.20  -t cimg/php:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.20-node  -t cimg/php:8.0-node .
-docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.20-browsers  -t cimg/php:8.0-browsers .
-docker build --file 8.1/Dockerfile -t cimg/php:8.1.7  -t cimg/php:8.1 .
-docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.7-node  -t cimg/php:8.1-node .
-docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.7-browsers  -t cimg/php:8.1-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.22 -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.22-node -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.22-browsers -t cimg/php:8.0-browsers .
+docker build --file 8.1/Dockerfile -t cimg/php:8.1.9 -t cimg/php:8.1 .
+docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.9-node -t cimg/php:8.1-node .
+docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.9-browsers -t cimg/php:8.1-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,23 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/php:7.4.30
-docker push cimg/php:7.4
-docker push cimg/php:7.4.30-node
-docker push cimg/php:7.4-node
-docker push cimg/php:7.4.30-browsers
-docker push cimg/php:7.4-browsers
-
-docker push cimg/php:8.0.20
 docker push cimg/php:8.0
-docker push cimg/php:8.0.20-node
+docker push cimg/php:8.0.22
 docker push cimg/php:8.0-node
-docker push cimg/php:8.0.20-browsers
+docker push cimg/php:8.0.22-node
 docker push cimg/php:8.0-browsers
-
-docker push cimg/php:8.1.7
+docker push cimg/php:8.0.22-browsers
 docker push cimg/php:8.1
-docker push cimg/php:8.1.7-node
+docker push cimg/php:8.1.9
 docker push cimg/php:8.1-node
-docker push cimg/php:8.1.7-browsers
+docker push cimg/php:8.1.9-node
 docker push cimg/php:8.1-browsers
+docker push cimg/php:8.1.9-browsers
