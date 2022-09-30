0f38b8432db0e332f6c71052e1fa050feb98972b
Michael Rothstein <michael.rothstein@circleci.com>
2022-09-30 15:41:11 -0400

    Publish v8.0.24. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.23 -t cimg/php:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.23-node -t cimg/php:8.0-node .
-docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.23-browsers -t cimg/php:8.0-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.24 -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.24-node -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.24-browsers -t cimg/php:8.0-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/php:8.0
-docker push cimg/php:8.0.23
+docker push cimg/php:8.0.24
 docker push cimg/php:8.0-node
-docker push cimg/php:8.0.23-node
+docker push cimg/php:8.0.24-node
 docker push cimg/php:8.0-browsers
-docker push cimg/php:8.0.23-browsers
+docker push cimg/php:8.0.24-browsers
