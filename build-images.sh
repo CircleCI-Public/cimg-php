3dabc30a7a7934ccadf32d89b1a89134b65d8b41
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2022-10-03 11:16:23 -0400

    Publish v8.0.23. [release] (#184)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 7.4/Dockerfile -t cimg/php:7.4.32 -t cimg/php:7.4 .
-docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.32-node -t cimg/php:7.4-node .
-docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.32-browsers -t cimg/php:7.4-browsers .
-docker build --file 8.1/Dockerfile -t cimg/php:8.1.11 -t cimg/php:8.1 .
-docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.11-node -t cimg/php:8.1-node .
-docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.11-browsers -t cimg/php:8.1-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.23 -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.23-node -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.23-browsers -t cimg/php:8.0-browsers .
