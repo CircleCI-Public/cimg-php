3b360ccda1f20f4731c57a75a7ca0d37a55436ea
Jeff Chen <jeff.chen@circleci.com>
2022-11-02 15:34:40 -0700

    Publish v7.4.33 and v8.0.25. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
+docker build --file 7.4/Dockerfile -t cimg/php:7.4.33 -t cimg/php:7.4 .
+docker build --file 7.4/node/Dockerfile -t cimg/php:7.4.33-node -t cimg/php:7.4-node .
+docker build --file 7.4/browsers/Dockerfile -t cimg/php:7.4.33-browsers -t cimg/php:7.4-browsers .
 docker build --file 8.0/Dockerfile -t cimg/php:8.0.25 -t cimg/php:8.0 .
 docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.25-node -t cimg/php:8.0-node .
 docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.25-browsers -t cimg/php:8.0-browsers .
-docker build --file 8.1/Dockerfile -t cimg/php:8.1.12 -t cimg/php:8.1 .
-docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.12-node -t cimg/php:8.1-node .
-docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.12-browsers -t cimg/php:8.1-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/php:7.4
+docker push cimg/php:7.4.33
+docker push cimg/php:7.4-node
+docker push cimg/php:7.4.33-node
+docker push cimg/php:7.4-browsers
+docker push cimg/php:7.4.33-browsers
 docker push cimg/php:8.0
 docker push cimg/php:8.0.25
 docker push cimg/php:8.0-node
 docker push cimg/php:8.0.25-node
 docker push cimg/php:8.0-browsers
 docker push cimg/php:8.0.25-browsers
-docker push cimg/php:8.1
-docker push cimg/php:8.1.12
-docker push cimg/php:8.1-node
-docker push cimg/php:8.1.12-node
-docker push cimg/php:8.1-browsers
-docker push cimg/php:8.1.12-browsers
