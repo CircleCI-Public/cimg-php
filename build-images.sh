e0fadb45d83c7c51b1540357c47f5bba112e2474
Jalexchen <jeff.chen@circleci.com>
2022-11-29 13:06:21 -0800

    Publish v8.1.13. [release] (#198)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.26 -t cimg/php:8.0 .
-docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.26-node -t cimg/php:8.0-node .
-docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.26-browsers -t cimg/php:8.0-browsers .
+docker build --file 8.1/Dockerfile -t cimg/php:8.1.13 -t cimg/php:8.1 .
+docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.13-node -t cimg/php:8.1-node .
+docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.13-browsers -t cimg/php:8.1-browsers .
