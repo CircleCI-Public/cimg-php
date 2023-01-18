d7f050fd165ad19594d5c19ebc62470b45713712
Jeff Chen <jeff.chen@circleci.com>
2023-01-18 15:32:10 -0800

    Publish v8.0.27. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.1/Dockerfile -t cimg/php:8.1.14 -t cimg/php:8.1 .
-docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.14-node -t cimg/php:8.1-node .
-docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.14-browsers -t cimg/php:8.1-browsers .
-docker build --file 8.2/Dockerfile -t cimg/php:8.2.1 -t cimg/php:8.2 .
-docker build --file 8.2/node/Dockerfile -t cimg/php:8.2.1-node -t cimg/php:8.2-node .
-docker build --file 8.2/browsers/Dockerfile -t cimg/php:8.2.1-browsers -t cimg/php:8.2-browsers .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.27 -t cimg/php:8.0 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.27-node -t cimg/php:8.0-node .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.27-browsers -t cimg/php:8.0-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/php:8.1
-docker push cimg/php:8.1.14
-docker push cimg/php:8.1-node
-docker push cimg/php:8.1.14-node
-docker push cimg/php:8.1-browsers
-docker push cimg/php:8.1.14-browsers
-docker push cimg/php:8.2
-docker push cimg/php:8.2.1
-docker push cimg/php:8.2-node
-docker push cimg/php:8.2.1-node
-docker push cimg/php:8.2-browsers
-docker push cimg/php:8.2.1-browsers
+docker push cimg/php:8.0
+docker push cimg/php:8.0.27
+docker push cimg/php:8.0-node
+docker push cimg/php:8.0.27-node
+docker push cimg/php:8.0-browsers
+docker push cimg/php:8.0.27-browsers
