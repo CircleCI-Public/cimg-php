a81e0db61433160c54945744620ab7bbedd22130
Jalexchen <jeff.chen@circleci.com>
2023-02-15 02:23:40 -0800

    Publish v8.1.15 and v8.2.2. [release] (#207)

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/php:8.0
-docker push cimg/php:8.0.27
-docker push cimg/php:8.0-node
-docker push cimg/php:8.0.27-node
-docker push cimg/php:8.0-browsers
-docker push cimg/php:8.0.27-browsers
+docker push cimg/php:8.1
+docker push cimg/php:8.1.15
+docker push cimg/php:8.1-node
+docker push cimg/php:8.1.15-node
+docker push cimg/php:8.1-browsers
+docker push cimg/php:8.1.15-browsers
+docker push cimg/php:8.2
+docker push cimg/php:8.2.2
+docker push cimg/php:8.2-node
+docker push cimg/php:8.2.2-node
+docker push cimg/php:8.2-browsers
+docker push cimg/php:8.2.2-browsers
