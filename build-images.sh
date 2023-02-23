e8b11b5b273f5bcad63fcbeab3fa71dba2baf141
Jalexchen <jeff.chen@circleci.com>
2023-02-23 08:18:25 -0800

    Publish v8.0.28. [release] (#213)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.1/Dockerfile -t cimg/php:8.1.16 -t cimg/php:8.1 --platform linux/amd64 .
-docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.16-node -t cimg/php:8.1-node --platform linux/amd64 .
-docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.16-browsers -t cimg/php:8.1-browsers --platform linux/amd64 .
-docker build --file 8.2/Dockerfile -t cimg/php:8.2.3 -t cimg/php:8.2 --platform linux/amd64 .
-docker build --file 8.2/node/Dockerfile -t cimg/php:8.2.3-node -t cimg/php:8.2-node --platform linux/amd64 .
-docker build --file 8.2/browsers/Dockerfile -t cimg/php:8.2.3-browsers -t cimg/php:8.2-browsers --platform linux/amd64 .
+docker build --file 8.0/Dockerfile -t cimg/php:8.0.28 -t cimg/php:8.0 --platform linux/amd64 .
+docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.28-node -t cimg/php:8.0-node --platform linux/amd64 .
+docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.28-browsers -t cimg/php:8.0-browsers --platform linux/amd64 .
