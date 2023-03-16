8bd537703622aaace3342844fd4873201da8a7c9
Jeff Chen <jeff.chen@circleci.com>
2023-03-16 09:43:39 -0700

    Publish v8.1.17 and v8.2.4. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 8.0/Dockerfile -t cimg/php:8.0.28 -t cimg/php:8.0 --platform linux/amd64 .
-docker build --file 8.0/node/Dockerfile -t cimg/php:8.0.28-node -t cimg/php:8.0-node --platform linux/amd64 .
-docker build --file 8.0/browsers/Dockerfile -t cimg/php:8.0.28-browsers -t cimg/php:8.0-browsers --platform linux/amd64 .
+docker build --file 8.1/Dockerfile -t cimg/php:8.1.17 -t cimg/php:8.1 --platform linux/amd64 .
+docker build --file 8.1/node/Dockerfile -t cimg/php:8.1.17-node -t cimg/php:8.1-node --platform linux/amd64 .
+docker build --file 8.1/browsers/Dockerfile -t cimg/php:8.1.17-browsers -t cimg/php:8.1-browsers --platform linux/amd64 .
+docker build --file 8.2/Dockerfile -t cimg/php:8.2.4 -t cimg/php:8.2 --platform linux/amd64 .
+docker build --file 8.2/node/Dockerfile -t cimg/php:8.2.4-node -t cimg/php:8.2-node --platform linux/amd64 .
+docker build --file 8.2/browsers/Dockerfile -t cimg/php:8.2.4-browsers -t cimg/php:8.2-browsers --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/php:8.0
-docker push cimg/php:8.0.28
-docker push cimg/php:8.0-node
-docker push cimg/php:8.0.28-node
-docker push cimg/php:8.0-browsers
-docker push cimg/php:8.0.28-browsers
+docker push cimg/php:8.1
+docker push cimg/php:8.1.17
+docker push cimg/php:8.1-node
+docker push cimg/php:8.1.17-node
+docker push cimg/php:8.1-browsers
+docker push cimg/php:8.1.17-browsers
+docker push cimg/php:8.2
+docker push cimg/php:8.2.4
+docker push cimg/php:8.2-node
+docker push cimg/php:8.2.4-node
+docker push cimg/php:8.2-browsers
+docker push cimg/php:8.2.4-browsers
