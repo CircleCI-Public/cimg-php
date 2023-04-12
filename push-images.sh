28154d270f610dd244828410e7476f5a5e3653e7
Jalexchen <jeff.chen@circleci.com>
2023-04-12 11:02:11 -0700

    Publish v8.2.5 and v8.1.18. [release] (#216)
    Co-authored-by: cpe-image-bot <secops+cpe-image-bot@circleci.com>

--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/php:8.1
-docker push cimg/php:8.1.17
+docker push cimg/php:8.1.18
 docker push cimg/php:8.1-node
-docker push cimg/php:8.1.17-node
+docker push cimg/php:8.1.18-node
 docker push cimg/php:8.1-browsers
-docker push cimg/php:8.1.17-browsers
+docker push cimg/php:8.1.18-browsers
 docker push cimg/php:8.2
-docker push cimg/php:8.2.4
+docker push cimg/php:8.2.5
 docker push cimg/php:8.2-node
-docker push cimg/php:8.2.4-node
+docker push cimg/php:8.2.5-node
 docker push cimg/php:8.2-browsers
-docker push cimg/php:8.2.4-browsers
+docker push cimg/php:8.2.5-browsers
